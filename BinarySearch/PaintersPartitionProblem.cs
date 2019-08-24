4a3e4ab409bf54e226292c2964731d48519a0347
Anil Kumar <anil.apex@outlook.com>
2019-08-25 01:23:07 +0530

    Organized files.

--- BinarySearch/PaintersPartitionProblem.cs
+++ BinarySearch/PaintersPartitionProblem.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using static System.Console;
 
-namespace Search
+namespace Search.BinarySearch
 {
     public class PaintersPartitionProblem
     {
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using Search.BinarySearch;
 
 namespace Search
 {
